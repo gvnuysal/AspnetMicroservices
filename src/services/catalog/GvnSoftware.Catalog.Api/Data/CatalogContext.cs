3cbdcd9d2eb94b6bbf470d6e47adf59901719aa9
guvenn <guven.uysal@hitsoft.com.tr>
2022-09-04 22:26:45 +0300

    Added seed data for catalog microservices

---
+++ src/services/catalog/GvnSoftware.Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using GvnSoftware.Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace GvnSoftware.Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client=new MongoClient(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(config.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(config.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; set; }
+    }
+}
---
+++ src/services/catalog/GvnSoftware.Catalog.Api/Data/ICatalogContext.cs
@@ -0,0 +1,8 @@
+﻿using GvnSoftware.Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace GvnSoftware.Catalog.Api.Data;
+public interface ICatalogContext
+{
+    IMongoCollection<Product> Products { get; }
+}
